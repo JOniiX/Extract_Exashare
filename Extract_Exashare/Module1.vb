ebb0e96809a49e733d9c293cc505f4cb85f2ed45
JOniiX <whitplayers@gmail.com>
2016-11-17 13:15:26 +0100

    Extract Exashare
    Extract MP4 link from exashare embed

---
+++ Extract_Exashare/Module1.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Text.RegularExpressions
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Enter exashare embed: ")
+        Dim link As String = Console.ReadLine() ' Exashare embed
+        Dim Rlink As String = Extract_Exashare(link) ' MP4 link
+        Console.WriteLine(Rlink)
+        Console.ReadKey()
+    End Sub
+
+    Private Function Extract_Exashare(url As String)
+        Dim request As WebRequest = WebRequest.Create(url)
+        Using response As WebResponse = request.GetResponse()
+            Using reader As New StreamReader(response.GetResponseStream())
+
+                Dim html As String = reader.ReadToEnd()
+                Dim regexL As Regex = New Regex("<iframe src=""(.*?)"" scrolling=")
+                Dim rL = regexL.Match(html)
+                Dim link As String = rL.Groups(1).ToString()
+
+                Dim request2 As WebRequest = WebRequest.Create(link)
+                Using response2 As WebResponse = request2.GetResponse()
+                    Using reader2 As New StreamReader(response2.GetResponseStream())
+
+                        Dim html2 As String = reader2.ReadToEnd()
+                        Dim regexRL As Regex = New Regex("file:""(.*?)"",label:")
+                        Dim rRL = regexRL.Match(html2)
+                        Dim Rlink As String = rRL.Groups(1).ToString()
+                        Return Rlink
+
+                    End Using
+                End Using
+
+            End Using
+        End Using
+    End Function
+
+End Module
---
+++ Extract_Exashare/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Passez en revue les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("Extract_Exashare")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Extract_Exashare")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("49839cb7-c910-40dc-b528-81ab2a90abd5")> 
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
